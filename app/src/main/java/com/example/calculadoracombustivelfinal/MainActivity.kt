5b15fd66c4c7fa5ca0570fa4f12cd76516e35bed
Luz <x7diogoluz@gmai.com>
2024-06-11 21:58:04 +0100

    Criação do butão que faz o calculo total e criação da função que faz os calculos necessários.

--- app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
+++ app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -118,7 +117,7 @@ fun CalculadoraLayout(name: String, modifier: Modifier = Modifier) {
             style = MaterialTheme.typography.bodyLarge
         )
         Spacer(modifier = Modifier.height(150.dp))
-        )
+
     }
 }
 
