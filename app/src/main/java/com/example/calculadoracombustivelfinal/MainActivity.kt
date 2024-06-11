b5fe83bbd2c2c355ad3db7fbb84cac8dd5447375
Luz <x7diogoluz@gmai.com>
2024-06-11 17:07:23 +0100

    Criação da função do Layout da calculadora e criação de uma coluna.

--- app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
+++ app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculadoracombustivelfinal.ui.theme.CalculadoraCombustivelFinalTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculadoraCombustivelFinalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    CalculadoraLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +39,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculadoraLayout(name: String, modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalculadoraCombustivelFinalTheme {
-        Greeting("Android")
+        CalculadoraLayout("Android")
     }
 }
