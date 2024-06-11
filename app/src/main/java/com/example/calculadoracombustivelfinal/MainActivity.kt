912925f6585b9e7bbd96c3983a07f758d88fcaa6
Luz <x7diogoluz@gmai.com>
2024-06-11 17:11:06 +0100

    Criação do Text onde vai aparecer o nome da aplicação.

--- app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
+++ app/src/main/java/com/example/calculadoracombustivelfinal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculadoracombustivelfinal.ui.theme.CalculadoraCombustivelFinalTheme
@@ -49,7 +51,12 @@ fun CalculadoraLayout(name: String, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier
+                .padding(bottom = 15.dp, top = 35.dp)
+                .align(alignment = Alignment.Start)
+        )
     }
 }
 
